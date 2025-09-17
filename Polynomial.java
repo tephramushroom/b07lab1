99a8a9fbc0b44cd961cb0fdc496b7f14c20ed65a
unknown <mushroomwlm@gmail.com>
2025-09-17 12:58:24 -0400

    added Polynomial.java and Driver.java

---
+++ Polynomial.java
@@ -0,0 +1,55 @@
+public class Polynomial {
+	double[] rep;
+	
+	public Polynomial() {
+		rep = new double[1];
+		rep[0] = 0;
+	}
+	
+	public Polynomial(double[] inp) {
+		rep = new double[inp.length];
+		for(int i = 0; i<inp.length; i++) {
+			rep[i] = inp[i];
+		}
+	}
+	
+	public Polynomial add(Polynomial another) {
+		int m;
+		if(rep.length>=another.rep.length) {
+			m=rep.length;
+		}
+		else {
+			m=another.rep.length;
+		}
+		double[] res = new double[m];
+		for(int i = 0; i<m; i++) {
+			double v1 = 0;
+			double v2 = 0;
+			if(i<rep.length) {
+				v1 = rep[i];
+			}
+			if(i<another.rep.length) {
+				v2 = another.rep[i];
+			}
+			res[i]=v1 + v2;
+		}
+		Polynomial s = new Polynomial(res);
+//		System.out.println(res[0]+"xx"+res[1]+"xx"+res[2]+"xx"+res[3]+"xx"+res[4]);
+		return s;
+	}
+	
+	public double evaluate(double x) {
+		double res = 0;
+		double xn = 1;
+		for(int i = 0; i<rep.length; i++) {
+			res += rep[i]*xn;
+			xn*=x;
+		}
+		return res;
+	}
+	
+	public boolean hasRoot(double r) {
+		double res = evaluate(r);
+		return (res==0);
+	}
+}
